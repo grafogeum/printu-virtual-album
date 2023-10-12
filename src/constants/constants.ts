45a73f124dd57ef391df7d46247b139f6c80de0c
michal <grafogeum@users.noreply.github.com>
2023-10-12 17:06:07 +0200

    fix: Improved HTTPS Encryption

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
 export const API_URL_BASE = "https://recruitment01.vercel.app/api/init";
-export const API_URL_PROJECT = "http://recruitment01.vercel.app/api/project/";
+export const API_URL_PROJECT = "https://recruitment01.vercel.app/api/project/";
